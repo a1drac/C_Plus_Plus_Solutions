5264b6a78208ec2b4a3a273e3a719a8091572fe2
a1drac <54896889+a1drac@users.noreply.github.com>
2019-09-04 22:38:14 +0630

    Update exercise_12.cpp

--- Chapter 3 Exercise_files/exercise_12.cpp
+++ Chapter 3 Exercise_files/exercise_12.cpp
@@ -1,21 +1,29 @@
 //Chapter 3 Solving Excercise Problem 12
 //exercise_12.cpp
 //Creating a four function calculator for fraction
-
 #include<iostream>
 using namespace std;
 int main()
 {
 	float a, b, c, d;
-	char option;
-	cout <<"\nEnter the first fraction: ";
-	cin >>a >>b;
+	char option,ch;
+	do
+    {
+
+	cout <<"Enter the first fraction: "<<endl;
+	cin >>a;cout <<"\n_"<<endl;cin>>b;
+
+
 
 	cout <<"\nEnter an operator(+, -, *, /): ";
 	cin >>option;
 
-	cout <<"\nEnter the second fraction: ";
-	cin >>c >>d;
+	cout <<"\nEnter the second fraction: "<<endl;
+	cin >>c;cout<<"\n_"<<endl;cin>>d
+	;
+
+
+
 
 	switch(option) {
 		case '+':
@@ -37,5 +45,10 @@ int main()
 		default:
 		cout <<"Invalid Option!";
 	}
-	return 0;
+	 cout << " Do another ?(y/n)";
+	 cin >>ch;
+    }
+    while(ch !='n');
+    return 0;
 }
+
