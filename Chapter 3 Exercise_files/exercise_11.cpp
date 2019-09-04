67441ac73eae8eb3515722ad2408ddb832eee804
a1drac <54896889+a1drac@users.noreply.github.com>
2019-09-04 21:43:48 +0630

    Update exercise_11.cpp

--- Chapter 3 Exercise_files/exercise_11.cpp
+++ Chapter 3 Exercise_files/exercise_11.cpp
@@ -4,35 +4,91 @@
 
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	float pound1, pound2, shilling1, shilling2, pence1, pence2;
-	char option;
-	cout <<"\nEnter first amount in old style english currency: \x9C";
-	cin >>pound1 >>shilling1 >>pence1;
+    float p1,p2,s1,s2,pe1,pe2,i;
+    char ch;
+    if(pe2>=12)
+    {
+        pe2-=12;
+        s2++;
+    }
+    if(s2>=20)
+    {
+        s2-=20;
+        p2++;
+    }
+    cout<< "Enter first amount  ";
+    cout<<"\npound:";
+    cin >> p1;cout<<"\nshilling:";cin>>s1;cout<<"\npence:";cin>>pe1;
+    cout << "   Choose an option "
+         << "\n + for addition of two amounts "
+         << "\n - for subtraction of amounts "
+         << "\n * for multiplication with a floating point"<<endl;
+    cin >> ch ;
 
-	cout <<"\nEnter second amount in old style english currency: \x9C";
-	cin >>pound2 >>shilling2 >>pence2;
+    switch(ch)
+    {
+    case '+':
+        cout<< "Enter second amount :";
+        cout<<"\npound:";
+        cin >> p2;cout<<"\nshilling:";cin>>s2;cout<<"\npence:";cin>>pe2;
+        p2+=p1;
+        s2+=s1;
+        pe2+=pe1;
+        if(pe2>=12)
+          {
+            pe2-=12;
+            s2++;
+          }
+       if(s2>=20)
+         {
+           s2-=20;
+           p2++;
+         }
 
-	cout <<"\nPress '+' for addition, press '-' for subtraction & press '*' for multiplication: ";
-	cin >>option;
+        cout<<"Total";
+        cout<<"\npound:"
+         << p2<<"\nshilling:"<<s2<<"\npence:"<<pe2;
+        break;
 
-	switch(option) {
-		case '+':
-		cout <<"Addition of two old style english currency is: \x9C" <<pound1+pound2 <<"." <<shilling1+shilling2 <<"." <<pence1+pence2 <<endl;
-		break;
+    case '-':
+        cout<< "Enter second amount :";
+        cout<<"\npound:";
+        cin >> p2;cout<<"\nshilling:";cin>>s2;cout<<"\npence:";cin>>pe2;
+        p2-=p1;
+        s2-=s1;
+        pe2-=pe1;
+        cout<<"Total";
+        cout<<"\npound:"
+         << p2<<"\nshilling:"<<s2<<"\npence:"<<pe2;
+        break;
 
-		case '-':
-		cout <<"subtraction of two old style english currency is: \x9C" <<pound1-pound2 <<"." <<shilling1-shilling2 <<"." <<pence1-pence2 <<endl;
-		break;
+    case '*':
+        cout<<"Enter floating point : " ; cin >> i ;
+        p2= p1 * i;
+        s2= s1 * i;
+        pe2= pe2 * i;
+         if(pe2>=12)
+           {
+            pe2-=12;
+            s2++;
+           }
+         if(s2>=20)
+           {
+           s2-=20;
+           p2++;
+           }
 
-		case '*':
-		cout <<"Multiplication of two old style englsih currency is: \x9C" <<pound1*pound2 <<"." <<shilling1*shilling2 <<"." <<pence1*pence2 <<endl;
-		break;
+        cout<<"Total";
+        cout<<"\npound:"
+         << p2<<"\nshilling:"<<s2<<"\npence:"<<pe2;
+        break;
+        
+    default ;
+    cout << "Invalid option !";
+    }
+    return 0;
 
-		default:
-		cout <<"Invalid Option!";
-
-	}
-	return 0;
 }
